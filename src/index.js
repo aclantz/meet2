206e1cd59b6d0bd9985aee31898dc4f88dd02b99
Alex Lantz <aclantz321@gmail.com>
2024-05-17 13:06:57 -0400

    add Atatus APM

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+//Atatus import, APM
+import * as atatus from 'atatus-spa';
+atatus.config('c453ca9df903477486f2cc919d4a9887').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +25,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
